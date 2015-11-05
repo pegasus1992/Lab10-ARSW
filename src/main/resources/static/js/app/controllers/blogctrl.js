e1a5b8a630f01650533c0e9dcffb0b1f66cb3c7e
jean9108 <joana9108@gmail.com>
2015-11-05 11:18:00 -0500

    parte 2b

--- src/main/resources/static/js/app/controllers/blogctrl.js
+++ src/main/resources/static/js/app/controllers/blogctrl.js
@@ -4,4 +4,26 @@ app.controller("blogCtrl", function($scope) {
     $scope.entry = {title : "Title",
                     content : "Content"};
     $log.debug('se creo el $scope');
+    $scope.loadData = function() {
+        $http({
+            method: "GET",
+            url: "blogs"
+        }).success(function(data) {
+            $scope.entries = data;
+        }).error(function(data,status,headers,config) {
+            alert("Ha fallado la petición. Estado HTTP:"+status);
+        });
+    };
+    $scope.loadData();
+    $scope.processForm = function() {
+        $log.debug($scope.entry);
+        $http({
+            method  : 'POST',
+            url     : 'blog',
+            data    : $scope.entry
+        }).success(function(data) {
+            console.log(data);
+            $scope.loadData();
+        });
+    };
 });
